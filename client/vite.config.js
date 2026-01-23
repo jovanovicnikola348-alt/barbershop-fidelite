d3cc9ee9362558b8de7059ba6a059fe8ab5f4969
jovanovicnikola348-alt <ton-email@gmail.com>
2026-01-23 01:13:43 +0100

    Fix Vercel routing issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Important pour Vercel
+  base: './', // IMPORTANT pour Vercel
   build: {
     outDir: 'dist',
-    sourcemap: false
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
   },
   server: {
     port: 3000
